38bdb8e3d76b6a2cfe36e85cf736bcd9ae778b1a
Jian Liew <jianloongliew@gmail.com>
2018-11-06 17:28:39 +1100

    Added 404

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom'
+import { Switch, BrowserRouter, Route } from 'react-router-dom'
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
@@ -28,15 +28,23 @@ const EditExpensePage = () => (
     </div>
 );
 
+const NotFoundPage = () => (
+    <div>
+        <p>404!</p>
+    </div>
+);
+
+
 
 const routes = (
     <BrowserRouter>
-    <div>
+    <Switch>
         <Route path="/" component={ExpenseDashboardPage} exact={true}/>
         <Route path="/create" component={AddExpensePage} />
         <Route path="/edit" component={EditExpensePage} />
         <Route path="/help" component={HelpPage} />
-    </div>
+        <Route component={NotFoundPage}  />
+    </Switch>
     </BrowserRouter>
 );
 
