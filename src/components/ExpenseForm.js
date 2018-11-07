543407d6d1eea4892ce935fc7ce58b179756261f
Jian Liew <jianloongliew@gmail.com>
2018-11-07 17:14:22 +1100

    Completed create expense feature

---
+++ src/components/ExpenseForm.js
@@ -0,0 +1,114 @@
+import React from "react";
+import moment from "moment";
+import { SingleDatePicker } from "react-dates";
+import "react-dates/lib/css/_datepicker.css";
+// This import needed to fix LTR issue
+import "react-dates/initialize";
+
+const now = moment();
+
+console.log(now.format("Do MMM, YYYY"));
+
+export default class ExpenseForm extends React.Component {
+  state = {
+    description: "",
+    note: "",
+    amount: "",
+    createdAt: moment(),
+    calendarFocused: false,
+    error: ""
+  };
+
+  onDescriptionChange = e => {
+    const description = e.target.value;
+    this.setState(() => ({ description }));
+  };
+
+  onNoteChange = e => {
+    const note = e.target.value;
+    this.setState(() => ({ note }));
+  };
+
+  onAmountChange = e => {
+    const amount = e.target.value;
+    // Using regular expression
+    if (!amount || amount.match(/^\d{1,}(\.\d{0,2})?$/))
+      this.setState(() => ({ amount }));
+  };
+
+  onDateChange = createdAt => {
+    if (createdAt) {
+      this.setState(() => {
+        createdAt;
+      });
+    }
+  };
+
+  onFocusChange = ({ focused }) => {
+    this.setState(() => ({ calendarFocused: focused }));
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+    if (!this.state.description || !this.state.amount) {
+      //Error message
+      this.setState(() => ({
+        error: "Please enter a description and amount"
+      }));
+    } else {
+      this.setState(() => ({
+        error: ""
+      }));
+      //Clear error
+      console.log("Test");
+      this.props.onSubmit({
+        description: this.state.description,
+        amount: parseFloat(this.state.amount, 10) * 100,
+        createdAt: this.state.createdAt.valueOf(),
+        note: this.state.note
+      });
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        {this.state.error && <p>{this.state.error}</p>}
+        <form onSubmit={this.onSubmit}>
+          <h3>Add Expense</h3>
+          <input
+            type="text"
+            placeholder="description"
+            value={this.state.description}
+            autoFocus
+            onChange={this.onDescriptionChange}
+          />
+          <input
+            type="text"
+            value={this.state.amount}
+            onChange={this.onAmountChange}
+            placeholder="amount"
+          />
+
+          <SingleDatePicker
+            date={this.state.createdAt} // momentPropTypes.momentObj or null
+            onDateChange={this.onDateChange} // PropTypes.func.isRequired
+            focused={this.state.calendarFocused} // PropTypes.bool
+            onFocusChange={this.onFocusChange} // PropTypes.func.isRequired
+            numberOfMonths={1}
+            isOutsideRange={day => false} //Everyday is OK
+            id="your_unique_id" // PropTypes.string.isRequired,
+          />
+
+          <textarea
+            value={this.state.note}
+            placeholder="Add a note (optional)"
+            onChange={this.onNoteChange}
+          />
+
+          <button>Add Expense</button>
+        </form>
+      </div>
+    );
+  }
+}
