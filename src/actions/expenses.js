9330807865f37c01813d49ac810a2e749e73f6e0
Jian Liew <jianloongliew@gmail.com>
2018-11-07 11:46:20 +1100

    Refactored redux reducers and actions

---
+++ src/actions/expenses.js
@@ -0,0 +1,27 @@
+import { v1 as uuid } from "uuid";
+
+export const editExpense = (id, updates) => ({
+  type: "EDIT_EXEPENSE",
+  updates
+});
+
+export const removeExpense = ({ id } = {}) => ({
+  type: "REMOVE_EXPENSE",
+  id
+});
+
+export const addExpense = ({
+  description = "",
+  note = "",
+  amount = 0,
+  createdAt = 0
+} = {}) => ({
+  type: "ADD_EXPENSE",
+  expense: {
+    id: uuid(),
+    description,
+    note,
+    amount,
+    createdAt
+  }
+});
--- src/app.js
+++ src/app.js
@@ -1,9 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom';
-import { NavLink, Link, Switch, BrowserRouter, Route } from 'react-router-dom'
-import AppRouter from './routers/AppRouter'
+import React from "react";
+import ReactDOM from "react-dom";
+import AppRouter from "./routers/AppRouter";
+import configureStore from "./store/configureStore";
+import { addExpense } from "./actions/expenses";
+import { setTextFilter } from "./actions/filters";
+import getVisibleExpenses from "./selectors/expenses";
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
 
+const store = configureStore();
+
+store.dispatch(addExpense({ description: "Water" }));
+
+store.dispatch(addExpense({ description: "Gas Bill" }));
+
+store.dispatch(setTextFilter("potato"));
+
+console.log(store.getState());
+
+const state = store.getState();
+
+const expenses = getVisibleExpenses(state.expenses, state.filters);
+
+console.log(expenses);
 
 ReactDOM.render(<AppRouter />, document.getElementById("app"));
